56715e1fb35ca52318da2b23b0bc1affe1ceb2b5
Isabelly Dias <isabellydias29@gmail.com>
2021-06-02 10:52:15 -0300

    Aula 2.2 -> Utilizando estruturas condicionais

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -1,4 +1,5 @@
 fun main() {
+
     println("Bem vindo ao bytebank")
     val titular: String = "Isabelly Dias"
     val numeroConta: Int = 1000
@@ -9,4 +10,25 @@ fun main() {
     println("Titular $titular")
     println("Número de conta $numeroConta")
     println("Saldo da conta $saldo")
+
+    testaCondicoes(saldo)
+}
+
+fun testaCondicoes(saldo: Double) {
+
+//    if (saldo > 0.0){
+//        println("Conta é positiva")
+//    }
+//    else if (saldo == 0.0){
+//        println("Conta é neutra")
+//    }
+//    else{
+//        println("Conta é negativa")
+//    }
+
+    when {
+        saldo > 0.0 -> println("Conta é positiva")
+        saldo == 0.0 -> println("Conta é neutra")
+        else -> println("Conta é negativa")
+    }
 }
