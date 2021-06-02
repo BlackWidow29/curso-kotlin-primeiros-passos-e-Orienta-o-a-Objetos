eb1c93191a9653dc6497d6132127d30b8e1c9483
Isabelly Dias <isabellydias29@gmail.com>
2021-06-02 09:46:43 -0300

    Aula 1 - Introdução

---
+++ bytebank/src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Bem vindo ao bytebank")
+    val titular: String = "Isabelly Dias"
+    val numeroConta: Int = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("Titular $titular")
+    println("Número de conta $numeroConta")
+    println("Saldo da conta $saldo")
+}
